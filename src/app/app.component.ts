6cc6cf41021a0b7240a245c11053ee3e59020dae
guy <ambar.guy@gmail.com>
2018-05-09 12:11:59 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {WeatherService} from "./weather.service";
+import {City} from "./city";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Open weather';
+  cities: Array<City>;
+  loading:boolean = true;
+  bestTemp = 21;
+  bestHumidity = 50;
+
+  constructor(private weatherService: WeatherService) {
+    this.weatherService.fetchAll().then((res) => {
+      this.cities = res['list'];
+      this.sortBestWeather();
+
+    });
+  }
+
+  toggleTemp(val) {
+    this.bestTemp = val;
+    this.loading = true;
+    this.sortBestWeather();
+  }
+
+  private sortBestWeather() {
+    this.cities.sort((a, b) => {
+      var distance1 = Math.abs(this.bestTemp - a.main.temp) + Math.abs(this.bestHumidity - a.main.humidity);
+      var distance2 = Math.abs(this.bestTemp - b.main.temp) + Math.abs(this.bestHumidity - b.main.humidity)
+
+      return distance1 == distance2 ? 0 : (distance1 > distance2 ? 1 : -1);
+    });
+
+    this.loading = false;
+  }
 }
